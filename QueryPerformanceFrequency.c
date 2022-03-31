c7a9ce8b6b072559d5196987df267e2f1233fe6f
dixyes <dixyes@gmail.com>
2022-03-31 09:59:21 +0800

    wip

---
+++ QueryPerformanceFrequency.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+int main() {
+    LARGE_INTEGER perf_frequency;
+    BOOL ret = QueryPerformanceFrequency(&perf_frequency);
+    printf("qpf: ret %d, freq %zu\n", ret, perf_frequency.QuadPart);
+    return 0;
+}
